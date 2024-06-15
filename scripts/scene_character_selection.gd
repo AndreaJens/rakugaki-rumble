050e9c071b7061c30ecefcb187a705895a4217f4
Andrea Demetrio <andrea.demetrio@hotmail.it>
2024-06-15 12:24:54 +0200

    First WIP structure for Whitechapel (still no projectiles)

--- scripts/scene_character_selection.gd
+++ scripts/scene_character_selection.gd
@@ -23,11 +23,12 @@ var mapOptions = {
 	0 : "chara_naomi",
 	1 : "chara_rhozetta",
 	2 : "chara_gridd",
+	3 : "chara_whitechapel",
 }
 
 func _ready():
-	_menu_p1.options = [0, 1, 2]
-	_menu_p2.options = [0, 1, 2]
+	_menu_p1.options = [0, 1, 2, 3]
+	_menu_p2.options = [0, 1, 2, 3]
 	if additionalSceneStartupParameters.has(AdditionalSceneCharacterSelectStartupParameter.Player1DeviceId):
 		player1DeviceId = additionalSceneStartupParameters[AdditionalSceneCharacterSelectStartupParameter.Player1DeviceId]
 	if additionalSceneStartupParameters.has(AdditionalSceneCharacterSelectStartupParameter.Player2DeviceId):
