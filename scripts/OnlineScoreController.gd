bbe57eb46dc7d5a61e153b0963ac6792a924fbe0
Andrea Demetrio <andrea.demetrio@hotmail.it>
2024-06-18 21:36:33 +0200

    Add FPS counter

--- scripts/OnlineScoreController.gd
+++ scripts/OnlineScoreController.gd
@@ -11,6 +11,7 @@ func _process(_delta):
 	if visible and NetworkAssistant.onlineScores.has(dictionaryKey):
 		$Player1Score.text = str(NetworkAssistant.onlineScores[dictionaryKey][0])
 		$Player2Score.text = str(NetworkAssistant.onlineScores[dictionaryKey][1])
-
+		$FPS.text = "FPS: %s" % Performance.get_monitor(Performance.TIME_FPS)
+	
 func set_dictionary_key(newKey : String):
 	dictionaryKey = newKey
